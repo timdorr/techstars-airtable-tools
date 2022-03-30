f33532ed4648a4648c812075fa324ca2867bd2c0
Tim Dorr <git@timdorr.com>
2022-03-29 23:55:08 -0400

    Also add an XLSX loader for the F6S data

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { config } from 'dotenv-flow'
 config()
 
+import { read } from 'xlsx'
 import Airtable from 'airtable'
 
 const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE as string)
