64741fbf6544a6f13ec9f3d922f1773b4ae7a4b9
Tim Dorr <git@timdorr.com>
2022-03-31 22:23:53 -0400

    Sync Companies

---
+++ src/f6s.ts
@@ -0,0 +1,132 @@
+export interface F6SCompany {
+  'Application ID': string
+  'Startup/Person Name': string
+  'Primary Contact Name': string
+  'Primary Contact Title': string
+  'Primary Contact Email Address': string
+  Status: string
+  'Complete %': string
+  'Date Created': string
+  'Time Created': string
+  'Date Finalized': string
+  'Time Finalized': string
+  'First Name': string
+  'Last Name': string
+  'Startup or Person': string
+  'User ID': string
+  City: string
+  Country: string
+  'Brief Description': string
+  'Skills or Markets': string
+  Website: string
+  Facebook: string
+  Twitter: string
+  Linkedin: string
+  Github: string
+  'Additional Link 1': string
+  'Additional Link 2': string
+  'Additional Link 3': string
+  'Product Video': string
+  'Team Video': string
+  'When did you start this company?': string
+  'Are you registered or incorporated?': string
+  'Where are you registered or incorporated?': string
+  'What do you do in detail?': string
+  "What's different/interesting about your Startup?": string
+  'How far along are you?': string
+  'How much money raised since start?': string
+  'How much runway do you have left?': string
+  'How is Equity divided?': string
+  'Key customers/users?': string
+  Raising: string
+  'Amount Raising': string
+  'Amount Currency': string
+  Valuation: string
+  'Valuation Currency': string
+  'Fund Stage': string
+  'Person 1 Name': string
+  'Person 1 Surname': string
+  'Person 1 User ID': string
+  'Person 1 City': string
+  'Person 1 Country': string
+  'Person 1 Brief Description': string
+  'Person 1 Skills or Markets': string
+  'Person 1 Facebook': string
+  'Person 1 Linkedin': string
+  'Person 1 Twitter': string
+  'Person 1 Website': string
+  'Person 1 Github': string
+  'Person 1 Email': string
+  'Person 1 Role': string
+  'Person 1 Skype': string
+  'Person 1 Phone': string
+  'Person 1 Experience': string
+  'Person 2 Name': string
+  'Person 2 Surname': string
+  'Person 2 User ID': string
+  'Person 2 City': string
+  'Person 2 Country': string
+  'Person 2 Brief Description': string
+  'Person 2 Skills or Markets': string
+  'Person 2 Facebook': string
+  'Person 2 Linkedin': string
+  'Person 2 Twitter': string
+  'Person 2 Website': string
+  'Person 2 Github': string
+  'Person 2 Email': string
+  'Person 2 Role': string
+  'Person 2 Skype': string
+  'Person 2 Phone': string
+  'Person 2 Experience': string
+  'Person 3 Name': string
+  'Person 3 Surname': string
+  'Person 3 User ID': string
+  'Person 3 City': string
+  'Person 3 Country': string
+  'Person 3 Brief Description': string
+  'Person 3 Skills or Markets': string
+  'Person 3 Facebook': string
+  'Person 3 Linkedin': string
+  'Person 3 Twitter': string
+  'Person 3 Website': string
+  'Person 3 Github': string
+  'Person 3 Email': string
+  'Person 3 Role': string
+  'Person 3 Skype': string
+  'Person 3 Phone': string
+  'Person 3 Experience': string
+  'Person 4 Name': string
+  'Person 4 Surname': string
+  'Person 4 User ID': string
+  'Person 4 City': string
+  'Person 4 Country': string
+  'Person 4 Brief Description': string
+  'Person 4 Skills or Markets': string
+  'Person 4 Facebook': string
+  'Person 4 Linkedin': string
+  'Person 4 Twitter': string
+  'Person 4 Website': string
+  'Person 4 Github': string
+  'Person 4 Email': string
+  'Person 4 Role': string
+  'Person 4 Skype': string
+  'Person 4 Phone': string
+  'Person 4 Experience': string
+  'Person 5 Name': string
+  'Person 5 Surname': string
+  'Person 5 User ID': string
+  'Person 5 City': string
+  'Person 5 Country': string
+  'Person 5 Brief Description': string
+  'Person 5 Skills or Markets': string
+  'Person 5 Facebook': string
+  'Person 5 Linkedin': string
+  'Person 5 Twitter': string
+  'Person 5 Website': string
+  'Person 5 Github': string
+  'Person 5 Email': string
+  'Person 5 Role': string
+  'Person 5 Skype': string
+  'Person 5 Phone': string
+  'Person 5 Experience': string
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,75 @@
 import { config } from 'dotenv-flow'
 config()
 
-import { read } from 'xlsx'
-import Airtable from 'airtable'
+import XLSX from 'xlsx'
+import Airtable, { FieldSet, Records } from 'airtable'
+
+import { F6SCompany } from './f6s'
+
+const sheet = XLSX.readFile(process.argv.slice(-1)[0])
+const apps = XLSX.utils.sheet_to_json<F6SCompany>(sheet.Sheets[sheet.SheetNames[1]], { range: 1 })
 
 const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE as string)
+
+async function main() {
+  try {
+    let records: Records<FieldSet> = []
+
+    for (const app of apps) {
+      console.log(`Processing application for ${app['Startup/Person Name']}`)
+
+      records = await airtable('Companies')
+        .select({ filterByFormula: `{F6S Company ID} = '${app['User ID']}'` })
+        .firstPage()
+
+      if (records.length == 0) {
+        await airtable('Companies').create({
+          'F6S Company ID': parseInt(app['User ID']),
+          Name: app['Startup/Person Name'],
+          Description: app['Brief Description'],
+          'Product Video': app['Product Video'],
+          'Team Video': app['Team Video'],
+          Location: `${app.City}${app.Country && app.Country != 'United States' ? `, ${app.Country}` : ''}`,
+          Website: app.Website,
+          Facebook: app.Facebook,
+          Twitter: app.Twitter,
+          Linkedin: app.Linkedin,
+          Incorporated: app['Are you registered or incorporated?'] == 'Yes',
+          'Where Incorporated': app['Where are you registered or incorporated?'],
+          'How Far Along': app['How far along are you?'],
+          'Money Raised': parseInt(app['How much money raised since start?'].replace(/\D/g, '')),
+          'Key Customers': app['Key customers/users?'],
+          Raising: app.Raising == 'Yes',
+          'Amount Raising': parseInt(app['Amount Raising']),
+          Valuation: parseInt(app.Valuation),
+          'Fund Stage': app['Fund Stage']
+        })
+      } else {
+        await airtable('Companies').update(records[0].id, {
+          Name: app['Startup/Person Name'],
+          Description: app['Brief Description'],
+          'Product Video': app['Product Video'],
+          'Team Video': app['Team Video'],
+          Location: `${app.City}${app.Country && app.Country != 'United States' ? `, ${app.Country}` : ''}`,
+          Website: app.Website,
+          Facebook: app.Facebook,
+          Twitter: app.Twitter,
+          Linkedin: app.Linkedin,
+          Incorporated: app['Are you registered or incorporated?'] == 'Yes',
+          'Where Incorporated': app['Where are you registered or incorporated?'],
+          'How Far Along': app['How far along are you?'],
+          'Money Raised': parseInt(app['How much money raised since start?'].replace(/\D/g, '')),
+          'Key Customers': app['Key customers/users?'],
+          Raising: app.Raising == 'Yes',
+          'Amount Raising': parseInt(app['Amount Raising']),
+          Valuation: parseInt(app.Valuation),
+          'Fund Stage': app['Fund Stage']
+        })
+      }
+    }
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+main()
