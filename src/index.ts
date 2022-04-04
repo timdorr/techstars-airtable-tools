ca18018483cf5058dae7f6d7bf3a199bd973a113
Tim Dorr <git@timdorr.com>
2022-04-04 11:00:11 -0400

    Move up the try block for less indentation

--- src/index.ts
+++ src/index.ts
@@ -13,98 +13,95 @@ const apps = XLSX.utils.sheet_to_json<F6SCompany>(sheet.Sheets[sheet.SheetNames[
 const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE as string)
 
 async function upsert(table: string, keyField: string, keyValue: string | number, fields: FieldSet): Promise<string> {
-  const records = await airtable(table)
-    .select({ filterByFormula: `{${keyField}} = '${keyValue}'` })
-    .firstPage()
+  try {
+    const records = await airtable(table)
+      .select({ filterByFormula: `{${keyField}} = '${keyValue}'` })
+      .firstPage()
 
-  if (records.length == 0) {
-    const result = await airtable(table).create(
-      {
-        [keyField]: keyValue,
-        ...fields
-      },
-      { typecast: true }
-    )
+    if (records.length == 0) {
+      const result = await airtable(table).create(
+        {
+          [keyField]: keyValue,
+          ...fields
+        },
+        { typecast: true }
+      )
 
-    return result.id
-  } else {
-    const result = await airtable(table).update(records[0].id, fields, { typecast: true })
-    return result.id
+      return result.id
+    } else {
+      const result = await airtable(table).update(records[0].id, fields, { typecast: true })
+      return result.id
+    }
+  } catch (e) {
+    console.error(e)
+    return e instanceof Error ? e.message : ''
   }
 }
 
 async function main() {
-  try {
-    for (const app of apps) {
-      console.log(`Processing application for ${app['Startup/Person Name']}`)
+  for (const app of apps) {
+    console.log(`Processing application for ${app['Startup/Person Name']}`)
 
-      const company_id = await upsert('Companies', 'F6S Company ID', parseInt(app['User ID']), {
-        Name: app['Startup/Person Name'],
-        Description: app['Brief Description'],
-        'Product Video': app['Product Video'],
-        'Team Video': app['Team Video'],
-        Location: `${app.City}${app.Country && app.Country != 'United States' ? `, ${app.Country}` : ''}`,
-        Website: app.Website,
-        Facebook: app.Facebook,
-        Twitter: app.Twitter,
-        Linkedin: app.Linkedin,
-        Incorporated: app['Are you registered or incorporated?'] == 'Yes',
-        'Where Incorporated': app['Where are you registered or incorporated?'],
-        'How Far Along': app['How far along are you?'],
-        'Money Raised': parseInt(app['How much money raised since start?'].replace(/\D/g, '')),
-        'Key Customers': app['Key customers/users?'],
-        Raising: app.Raising == 'Yes',
-        'Amount Raising': parseInt(app['Amount Raising']),
-        Valuation: parseInt(app.Valuation),
-        'Fund Stage': app['Fund Stage']
+    const company_id = await upsert('Companies', 'F6S Company ID', parseInt(app['User ID']), {
+      Name: app['Startup/Person Name'],
+      Description: app['Brief Description'],
+      'Product Video': app['Product Video'],
+      'Team Video': app['Team Video'],
+      Location: `${app.City}${app.Country && app.Country != 'United States' ? `, ${app.Country}` : ''}`,
+      Website: app.Website,
+      Facebook: app.Facebook,
+      Twitter: app.Twitter,
+      Linkedin: app.Linkedin,
+      Incorporated: app['Are you registered or incorporated?'] == 'Yes',
+      'Where Incorporated': app['Where are you registered or incorporated?'],
+      'How Far Along': app['How far along are you?'],
+      'Money Raised': parseInt(app['How much money raised since start?'].replace(/\D/g, '')),
+      'Key Customers': app['Key customers/users?'],
+      Raising: app.Raising == 'Yes',
+      'Amount Raising': parseInt(app['Amount Raising']),
+      Valuation: parseInt(app.Valuation),
+      'Fund Stage': app['Fund Stage']
+    })
+
+    await upsert('Applications', 'F6S Application ID', parseInt(app['Application ID']), {
+      'Primary Contact Name': app['Primary Contact Name'],
+      'Primary Contact Title': app['Primary Contact Title'],
+      'Primary Contact Email': app['Primary Contact Email Address'],
+      Status: app.Status,
+      'Complete %': parseInt(app['Complete %']) / 100,
+      Program: sheet.SheetNames[1],
+      'Date Created': parse(
+        `${app['Date Created']} ${app['Time Created']}`,
+        'dd/MM/yyyy HH:mm:ss',
+        new Date()
+      ).toISOString(),
+      Company: [company_id]
+    })
+
+    await Array.from({ length: 5 }, async (_, n) => {
+      const num: PersonNumber = (n + 1) as PersonNumber
+
+      if (!app[`Person ${num} User ID`]) return
+      await upsert('Founders', 'F6S User ID', parseInt(app[`Person ${num} User ID`]), {
+        Name: `${app[`Person ${num} Name`]} ${app[`Person ${num} Surname`]}`,
+        Email: app[`Person ${num} Email`],
+        Phone: app[`Person ${num} Phone`],
+        Location: `${app[`Person ${num} City`]}${
+          app[`Person ${num} Country`] && app[`Person ${num} Country`] != 'United States'
+            ? `, ${app[`Person ${num} Country`]}`
+            : ''
+        }`,
+        Role: app[`Person ${num} Role`],
+        Skills: app[`Person ${num} Skills or Markets`].split(', ').filter(Boolean),
+        Facebook: app[`Person ${num} Facebook`],
+        Linkedin: app[`Person ${num} Linkedin`],
+        Twitter: app[`Person ${num} Twitter`],
+        Website: app[`Person ${num} Website`],
+        Description: app[`Person ${num} Brief Description`],
+        Experience: app[`Person ${num} Experience`],
+        Companies: [company_id]
       })
-
-      await upsert('Applications', 'F6S Application ID', parseInt(app['Application ID']), {
-        'Primary Contact Name': app['Primary Contact Name'],
-        'Primary Contact Title': app['Primary Contact Title'],
-        'Primary Contact Email': app['Primary Contact Email Address'],
-        Status: app.Status,
-        'Complete %': parseInt(app['Complete %']) / 100,
-        Program: sheet.SheetNames[1],
-        'Date Created': parse(
-          `${app['Date Created']} ${app['Time Created']}`,
-          'dd/MM/yyyy HH:mm:ss',
-          new Date()
-        ).toISOString(),
-        Company: [company_id]
-      })
-
-      await Array.from({ length: 5 }, async (_, n) => {
-        const num: PersonNumber = (n + 1) as PersonNumber
-
-        if (!app[`Person ${num} User ID`]) return
-        try {
-          await upsert('Founders', 'F6S User ID', parseInt(app[`Person ${num} User ID`]), {
-            Name: `${app[`Person ${num} Name`]} ${app[`Person ${num} Surname`]}`,
-            Email: app[`Person ${num} Email`],
-            Phone: app[`Person ${num} Phone`],
-            Location: `${app[`Person ${num} City`]}${
-              app[`Person ${num} Country`] && app[`Person ${num} Country`] != 'United States'
-                ? `, ${app[`Person ${num} Country`]}`
-                : ''
-            }`,
-            Role: app[`Person ${num} Role`],
-            Skills: app[`Person ${num} Skills or Markets`].split(', ').filter(Boolean),
-            Facebook: app[`Person ${num} Facebook`],
-            Linkedin: app[`Person ${num} Linkedin`],
-            Twitter: app[`Person ${num} Twitter`],
-            Website: app[`Person ${num} Website`],
-            Description: app[`Person ${num} Brief Description`],
-            Experience: app[`Person ${num} Experience`],
-            Companies: [company_id]
-          })
-        } catch (e) {
-          console.error(e)
-        }
-      })
-    }
-  } catch (e) {
-    console.error(e)
+    })
   }
 }
 
